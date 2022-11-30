7dcb39d6052317d6691ea86ecbc287a137e6d0bf
rickyputrasardika22 <primatara42@gmail.com>
2022-11-30 14:16:04 +0700

    Package_Update
    >>Menambahkan Package

---
+++ app/src/main/java/com/primaputraagusantara_if3b_pab_quotes/Activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.primaputraagusantara_if3b_pab_quotes.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.primaputraagusantara_if3b_pab_quotes.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/com/primaputraagusantara_if3b_pab_quotes/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.primaputraagusantara_if3b_pab_quotes;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
